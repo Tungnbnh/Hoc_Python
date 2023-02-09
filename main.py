4432390df3aa76f6d5d3103c0e182680b6f678ed
Your Name <you@example.com>
2023-02-09 21:06:28 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+number = [1, 2, 3, 4 ,5 ,6 ]
+number.append(10) #Thêm một phần tử vào cuối danh sách
+number.insert(1, 15) #Thêm một phần tử vào vị trí chỉ định
+tropical = [13,13,31]
+number.extend(tropical) # Các phần tử sẽ được thêm vào cuối danh sách
+number.remove(13) #Xoá phần tử đã chỉ định
+number.pop(3) #Xoá phần tử có chỉ mục đã đc chỉ định , nếu ko ghi chỉ mục thì mặc định xoá phần tử cuối cùng
+max_number = max(number)
+print(len(number))
+print(max_number)
